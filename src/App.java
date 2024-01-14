219df69e4d8513b0d6593975bebdcea6f933ae4e
Matheus da Silva <contagodzillajapan@gmail.com>
2024-01-14 18:27:26 -0300

    Classe que contém objetos que implementam a interface ou herdam da classe abstrata

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        RedeSocial facebook = new Facebook();
+        facebook.postagemTexto();
+
+        System.out.println("--------------------");
+
+        RedeSocial twitter = new Twitter();
+        twitter.fazerComentario();
+
+        System.out.println("--------------------");
+
+        RedeSocial instagram = new Instagram();
+        instagram.fazerLive();
+
+        System.out.println("--------------------");
     }
 }
+
+/*
+ * Classe que contém objetos que implementam a interface ou herdam da classe abstrata.
+ */
