c80a4709a8e22d14ff03153224dfb00614e673b0
Matheus da Silva <contagodzillajapan@gmail.com>
2024-01-14 18:26:17 -0300

    Desenvolvendo a classe concreta 3 - Instagram

---
+++ src/Instagram.java
@@ -0,0 +1,16 @@
+public class Instagram extends RedeSocial {
+    @Override
+    public void logar() {
+        System.out.println("Logando no Instagram");
+    }
+
+    @Override
+    public void fazerLive() {
+        System.out.println("Fazendo live no Instagram");
+    }
+}
+
+/*
+ * Fornecem implementações específicas para as etapas abstratas definidas na Classe Abstrata. 
+ * Personalizam o comportamento do algoritmo sem modificar sua estrutura global.
+ */
