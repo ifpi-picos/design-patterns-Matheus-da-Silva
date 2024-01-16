a415ff7531eadf3c81e0ea145a9df8462e387b28
Matheus da Silva <contagodzillajapan@gmail.com>
2024-01-16 16:26:13 -0300

    Adequação para o projeto

---
+++ src/RedeSocial1.java
@@ -0,0 +1,14 @@
+public class RedeSocial1 extends RedeSocial {
+    @Override 
+    public void postagem() {
+        String texto = ("Estou fazendo um comentário");
+    }
+}
+
+/*
+ * Esta classe fornece implementações específicas para as 
+ * etapas abstratas definidas na Classe Abstrata. 
+ * 
+ * Personalizam o comportamento do algoritmo 
+ * sem modificar sua estrutura global.
+ */
---
+++ src/RedeSocial2.java
@@ -0,0 +1,17 @@
+public class RedeSocial2 extends RedeSocial {
+    @Override 
+    public void postagem() {
+        String texto = ("Estou fazendo um comentário");
+        Image imagemPost = new Image("imagem.png");
+    }
+}
+
+/*
+ * Esta classe fornece implementações específicas para as 
+ * etapas abstratas definidas na Classe Abstrata. 
+ * 
+ * Que é diferente da implementação da classe concreta 1
+ * 
+ * Personalizam o comportamento do algoritmo 
+ * sem modificar sua estrutura global.
+ */
